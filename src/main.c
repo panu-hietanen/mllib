39d61ed2f785b4d740d7aa4a75e01e14f2a5700b
panu-hietanen <78078779+panu-hietanen@users.noreply.github.com>
2026-04-29 22:48:23 +0100

    Access tensor in one pass. Allow printing as flat array

--- src/main.c
+++ src/main.c
@@ -1,12 +1,17 @@
 #include "config.h"
 
 #include "arena.h"
+#include "tensor.h"
 
 int main()
 {
 	mem_arena* arena = arena_create(MiB(1));
 
-	arena_destroy(arena);
+	int shape[MAX_DIMS] = { 2, 2, 0, 0, 0, 0, 0, 0 };
+	Tensor* t = tensor_create(arena, shape, 2, false);
 
+	tensor_print(t);
+
+	arena_destroy(arena);
 	return 0;
 }
--- src/tensor.c
+++ src/tensor.c
@@ -16,15 +16,35 @@ Tensor* tensor_create(mem_arena* arena, int* shape, int ndim, bool non_zero)
 
 f32* tensor_at(const Tensor* t, int* indices)
 {
+	int stride = 1;
 	int idx = 0;
-	for (size_t i = 0; i < t->ndim; ++i)
+	for (int i = t->ndim - 1; i >= 0; --i)
 	{
-		int stride = 1;
-		for (size_t j = i + 1; j < t->ndim; ++j)
-		{
-			stride *= t->shape[j];
-		}
 		idx += indices[i] * stride;
+		stride *= t->shape[i];
 	}
 	return &t->data[idx];
+}
+
+void tensor_print(const Tensor* t)
+{
+	printf("Shape=(");
+	int elements = 1;
+	for (size_t i = 0; i < t->ndim; ++i)
+	{
+		elements *= t->shape[i];
+		if (i != t->ndim - 1)
+			printf("%d, ", t->shape[i]);
+		else
+			printf("%d", t->shape[i]);
+	}
+	printf("), data=[");
+	for (size_t i = 0; i < elements; ++i)
+	{
+		if (i != elements - 1)
+			printf("%f, ", t->data[i]);
+		else
+			printf("%f", t->data[i]);
+	}
+	printf("]\n");
 }
