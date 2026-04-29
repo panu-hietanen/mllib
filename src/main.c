cdb69cd586dff5ad62cf3957821610e8278ec489
panu-hietanen <78078779+panu-hietanen@users.noreply.github.com>
2026-04-29 23:12:39 +0100

    Testing in main

--- src/main.c
+++ src/main.c
@@ -7,8 +7,12 @@ int main()
 {
 	mem_arena* arena = arena_create(MiB(1));
 
-	int shape[MAX_DIMS] = { 2, 2, 0, 0, 0, 0, 0, 0 };
-	Tensor* t = tensor_create(arena, shape, 2, false);
+	int shape[MAX_DIMS] = { 2, 2, 2, 0, 0, 0, 0, 0 };
+	Tensor* t = tensor_create(arena, shape, 3, false);
+
+	tensor_print(t);
+
+	tensor_fill(t, 32);
 
 	tensor_print(t);
 
