79414cf43e8e0e2ecfa756713bc0d61b66843a36
panu-hietanen <78078779+panu-hietanen@users.noreply.github.com>
2026-05-04 20:35:16 +0100

    Add training loop for XOR

--- src/main.c
+++ src/main.c
@@ -4,38 +4,61 @@
 #include "arena.h"
 #include "tensor.h"
 #include "graph.h"
+#include "optimizer.h"
 
 int main()
 {
-	mem_arena* arena = arena_create(MiB(1));
+	mem_arena* arena_p = arena_create(MiB(1));
+	mem_arena* arena_t = arena_create(MiB(1));
 	srand(time(NULL));
 
-	i32 shape_x[]   = { 1, 2 };
+	i32 iters = 100000;
+	f32 lr = 1e-3;
+
+	i32 shape_x[]   = { 4, 2 };
 	i32 shape_w1[]  = { 2, 4 };
 	i32 shape_w2[]  = { 4, 1 };
-	i32 shape_target[] = { 1, 1 };
+	i32 shape_target[] = { 4, 1 };
 
-	Tensor* x = tensor_create(arena, shape_x, 2, true);
-	x->data[0] = 5;
-	x->data[1] = 3;
+	Tensor* x = tensor_create(arena_p, shape_x, 2, true);
+	x->data[0] = 0;
+	x->data[1] = 0;
+	x->data[2] = 0;
+	x->data[3] = 1;
+	x->data[4] = 1;
+	x->data[5] = 0;
+	x->data[6] = 1;
+	x->data[7] = 1;
 
-	Tensor* target = tensor_create(arena, shape_target, 2, true);
+	Tensor* target = tensor_create(arena_p, shape_target, 2, true);
+	target->data[0] = 0;
+	target->data[1] = 1;
+	target->data[2] = 1;
+	target->data[3] = 0;
 
-	Tensor* w1 = tensor_ones(arena, shape_w1, 2);
-	Tensor* w2 = tensor_ones(arena, shape_w2, 2);
+	Tensor* w1 = tensor_rand(arena_p, shape_w1, 2);
+	Tensor* w2 = tensor_rand(arena_p, shape_w2, 2);
+	Tensor* weights[2] = {w1, w2};
 
-	Tensor* h = graph_matmul(arena, x, w1);
-	Tensor* out = graph_matmul(arena, h, w2);
-	Tensor* loss = graph_mse(arena, out, target);
+	for (i32 it = 0; it < iters; ++it)
+	{
+		Tensor* h = graph_relu(arena_t, graph_matmul(arena_t, x, w1));
+		Tensor* out = graph_matmul(arena_t, h, w2);
+		Tensor* loss = graph_mse(arena_t, out, target);
 
-	tensor_print(x);
-	tensor_print(h);
+		if (it % 1000 == 0) tensor_print(loss);
+
+		backward(arena_t, loss);
+		step(weights, 2, lr);
+
+		arena_clear(arena_t);
+	}
+
+	Tensor* h = graph_relu(arena_t, graph_matmul(arena_t, x, w1));
+	Tensor* out = graph_matmul(arena_t, h, w2);
+
 	tensor_print(out);
 
-	backward(arena, out);
-
-	tensor_print(w1->grad);
-	tensor_print(w2->grad);
-
-	arena_destroy(arena);
+	arena_destroy(arena_p);
+	arena_destroy(arena_t);
 }
