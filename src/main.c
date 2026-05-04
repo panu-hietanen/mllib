15c47602c8585fd542b0abc6cee9cd1e0757c30c
panu-hietanen <78078779+panu-hietanen@users.noreply.github.com>
2026-05-04 21:52:38 +0100

    Use bias terms during training

--- src/main.c
+++ src/main.c
@@ -12,12 +12,14 @@ int main()
 	mem_arena* arena_t = arena_create(MiB(1));
 	srand(time(NULL));
 
-	i32 iters = 100000;
-	f32 lr = 1e-3;
+	i32 iters = 1000000;
+	f32 lr = 1e-2;
 
-	i32 shape_x[]   = { 4, 2 };
-	i32 shape_w1[]  = { 2, 4 };
-	i32 shape_w2[]  = { 4, 1 };
+	i32 shape_x[]      = { 4, 2 };
+	i32 shape_w1[]     = { 2, 4 };
+	i32 shape_w2[]     = { 4, 1 };
+	i32 shape_b1[]     = { 1, 4 };
+	i32 shape_b2[]     = { 1, 1 };
 	i32 shape_target[] = { 4, 1 };
 
 	Tensor* x = tensor_create(arena_p, shape_x, 2, true);
@@ -38,22 +40,36 @@ int main()
 
 	Tensor* w1 = tensor_rand(arena_p, shape_w1, 2);
 	Tensor* w2 = tensor_rand(arena_p, shape_w2, 2);
-	Tensor* weights[2] = {w1, w2};
+	Tensor* b1 = tensor_zeros(arena_p, shape_b1, 2);
+	Tensor* b2 = tensor_zeros(arena_p, shape_b2, 2);
+	Tensor* learnable[4] = {w1, w2, b1, b2 };
 
 	for (i32 it = 0; it < iters; ++it)
 	{
-		Tensor* h = graph_relu(arena_t, graph_matmul(arena_t, x, w1));
-		Tensor* out = graph_matmul(arena_t, h, w2);
+		Tensor* h = graph_relu(arena_t, graph_add(arena_t, graph_matmul(arena_t, x, w1), b1));
+		Tensor* out = graph_add(arena_t, graph_matmul(arena_t, h, w2), b2);
 		Tensor* loss = graph_mse(arena_t, out, target);
 
 		if (it % (iters / 10) == 0) tensor_print(loss);
 
 		backward(arena_t, loss);
-		step(weights, 2, lr);
+		step(learnable, 4, lr);
 
 		arena_clear(arena_t);
 	}
 
+	printf("\n==========================================\n");
+	printf("=================WEIGHTS==================");
+	printf("\n==========================================\n");
+
+	tensor_print(w1);
+	tensor_print(w2);
+	tensor_print(b1);
+	tensor_print(b2);
+
+	printf("\n==========================================\n");
+	printf("=================RESULTS==================");
+	printf("\n==========================================\n");
 	Tensor* h = graph_relu(arena_t, graph_matmul(arena_t, x, w1));
 	Tensor* out = graph_matmul(arena_t, h, w2);
 
