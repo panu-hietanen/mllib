17b3d906acf5370b9afed9a4249f9d663eb775b8
panu-hietanen <panu.hietanen@gmail.com>
2026-05-12 12:43:58 +0000

    Moved data.py and added functions for parsing training data csv

---
+++ python/mllib/data.py
@@ -0,0 +1,39 @@
+import numpy as np
+from numpy.typing import NDArray
+import pandas as pd
+
+import chess
+
+PIECE_TO_TYPE = {
+    chess.PAWN:   0,
+    chess.ROOK:   1,
+    chess.KNIGHT: 2,
+    chess.BISHOP: 3,
+    chess.QUEEN:  4,
+    chess.KING:   5,
+
+}
+FEATURES = 768
+
+def fen_to_features(fen: str) -> NDArray:
+    board = chess.Board(fen)
+    features = np.zeros(FEATURES, dtype=np.float32)
+    for idx, square in enumerate(chess.SQUARES):
+        piece = board.piece_at(square)
+        if piece:
+            colour_offset = 0 if piece.color == chess.WHITE else 6
+            piece_idx = PIECE_TO_TYPE[piece.piece_type] + colour_offset
+            feature_idx = piece_idx * 64 + idx
+            features[feature_idx] = 1.0
+    return features
+            
+def load_chunk(path: str, skip: int, n: int) -> tuple[NDArray, NDArray]:
+    df = pd.read_csv(path, skiprows=range(1, skip+1), nrows=n, header=0)
+    df_no_mate = df[~df["Evaluation"].str.startswith('#')]
+
+    X = np.array([fen_to_features(fen) for fen in df_no_mate["FEN"]])
+    
+    evals = df_no_mate["Evaluation"].astype(float)
+    y = (evals > 0).astype(np.float32).values.reshape(-1, 1)
+    
+    return X, y
--- python/training.py
+++ python/training.py
@@ -38,6 +38,12 @@ model = Model(
 batch_size = 64
 batches = n_datapoints // batch_size
 
+from mllib.data import load_chunk
+
+X, y = load_chunk("train_data/chessData.csv", skip=0, n=100)
+print(X.shape, y.shape)
+print(y[:5])
+
 indices = np.arange(0, n_datapoints)
 for epoch in range(epochs):
     epoch_loss = 0.0
